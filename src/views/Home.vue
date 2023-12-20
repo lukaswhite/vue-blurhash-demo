40006590fac7cb00ca1f8553210087edf9c6581f
Lukas White <hello@lukaswhite.com>
2023-12-20 10:06:00 +0000

    Changed image URL and added built site

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <fancy-image
-      :src="`https://images.unsplash.com/photo-1682687982360-3fbab65f9d50?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&cb=${cb}`"
+      :src="`https://raw.githubusercontent.com/lukaswhite/vue-blurhash-demo/main/assets/photo.jpg?cb=${cb}`"
       blurhash="LuBqYz%0MxRQ*0tQRPRk%Mt7R.WY"/>
   </div>
 </template>
